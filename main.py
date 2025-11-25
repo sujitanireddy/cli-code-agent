d4b9007e658431eafa69e781a812f8c59897d9f1
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2025-11-25 11:32:10 -0600

    LLM can now call get_files_info function

--- main.py
+++ main.py
@@ -4,29 +4,58 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+from call_function import available_functions
 
-if len(sys.argv) <= 1:
-    print('ai-agent')
-    print('Usage: python main.py "<prompt>"')
-    print("No prompt given. Exiting the program..")
-    exit(1)
 
-user_prompt = sys.argv[1]
+def main():
+    load_dotenv()
 
-#load api key from .env file
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
-#create a new instance of a Gemini client
-client = genai.Client(api_key=api_key)
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
 
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    user_prompt = " ".join(args)
 
-print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
-if '--verbose' in sys.argv: 
-    print(f'User prompt: {user_prompt}')
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+
+if __name__ == "__main__":
+    main()
